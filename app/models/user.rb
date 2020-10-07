4ada12c4383167aee1f9d45980bd4c9da9994f28
Tanay <tanaysharma2627@gmail.com>
2020-10-07 18:04:53 +0530

    update code

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   #Validations
-  validates :name,:user_type, presence: true
+  validates :name,:type, presence: true
 
   %w(Patient Doctor).each do |u_type|
     define_method "#{u_type.downcase}?" do
